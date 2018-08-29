421d266d86a530acd405778c60c7c7684e863669
Stephen Malek <malek.stephen44@gmail.com>
2018-08-29 11:03:20 -0600

    add presenter test for order by distance

--- spec/presenters/presenter_spec.rb
+++ spec/presenters/presenter_spec.rb
@@ -15,5 +15,17 @@ describe Presenter, type: :model do
       expect(station.distance).to eq(0.31422)
       expect(station.access_times).to eq("24 hours daily")
     end
+    it 'can order by distance' do
+
+      all_stations = FuelService.new("80203").closest_fuel_stations.map {|station_info| Station.new(station_info)}
+      presenter = Presenter.new("80203")
+      expected = presenter.sort_by_distance(all_stations)
+
+      expect(all_stations.count).to eq(20)
+      expect(expected.count).to eq(10)
+      
+      expect(expected.first.distance).to eq(0.31422)
+      expect(expected.last.distance).to eq(0.76279)
+    end
   end
 end
