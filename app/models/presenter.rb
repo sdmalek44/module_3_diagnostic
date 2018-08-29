47768370aa9072a460925d4a26baf785f65be142
Stephen Malek <malek.stephen44@gmail.com>
2018-08-29 10:11:08 -0600

    finally get json to give me what i want uri mistake

--- app/models/presenter.rb
+++ app/models/presenter.rb
@@ -4,11 +4,16 @@ class Presenter
   end
 
   def closest_fuel_stations
-    @conn =|| Faraday.new(url: 'https://developer.nrel.gov') do |faraday|
+    @conn ||= Faraday.new(url: 'https://developer.nrel.gov') do |faraday|
       faraday.headers["X-API-Key"] = ENV["API_KEY"]
       faraday.adapter Faraday.default_adapter
     end
-    response = conn.get("/v1/nearest.json?location=#{@zip}&fuel_type=ELEC,LPG")
-    JSON.parse(conn.get('').body, symbolize_names: true)
+    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&fuel_type=ELEC,LPG")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    json[:fuel_stations].map do |station_info|
+      Station.new(station_info)
+    end
   end
 end
