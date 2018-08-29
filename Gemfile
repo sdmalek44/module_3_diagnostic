088638930cf83133c5a8a5a9623bcdd2d9363fae
Stephen Malek <malek.stephen44@gmail.com>
2018-08-28 21:31:10 -0600

    add to gemfile

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'faraday'
 gem 'figaro'
+gem 'bcrypt'
 
 group :development, :test do
   gem 'byebug'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,12 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
@@ -46,6 +51,17 @@ RSpec.configure do |config|
   #       # ...
   #     end
   #
+  config.before(:suite) do
+   DatabaseCleaner.clean_with(:truncation)
+   DatabaseCleaner.strategy = :truncation
+  end
+
+   config.around(:each) do |example|
+     DatabaseCleaner.cleaning do
+       example.run
+     end
+   end
+
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
