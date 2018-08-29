e5a0ecf4b125894028d85fb2e6ea2376f0fc3859
Stephen Malek <malek.stephen44@gmail.com>
2018-08-29 10:40:50 -0600

    make conn and get json method

--- app/services/fuel_service.rb
+++ app/services/fuel_service.rb
@@ -5,12 +5,19 @@ class FuelService
   end
 
   def closest_fuel_stations
+    get_json("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&fuel_type=ELEC,LPG")[:fuel_stations]
+  end
+
+  private
+
+  def conn
     @conn ||= Faraday.new(url: 'https://developer.nrel.gov') do |faraday|
       faraday.headers["X-API-Key"] = ENV["API_KEY"]
       faraday.adapter Faraday.default_adapter
     end
-    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&fuel_type=ELEC,LPG")
+  end
 
-    JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
   end
 end
